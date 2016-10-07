a39e24b247feaa17d0b5df01670fc22066f95c98
rjopek <rafaljopek@hotmail.com>
2016-10-07 16:44:34 -0500

    added uiInit(), uses internal C initialization, not needed on Harbour

--- src/main.c
+++ src/main.c
@@ -2,6 +2,22 @@
 #include "hbapi.h"
 #include "ui.h"
 
+HB_FUNC( UIINIT ) {
+	uiInitOptions options;
+	const char *err;
+
+	memset(&options, 0, sizeof (uiInitOptions));
+	err = uiInit(&options);
+
+    if (err != NULL) {
+		fprintf(stderr, "error initializing libui: %s", err);
+		uiFreeInitError(err);
+		hb_retl( HB_TRUE );
+	}
+    else
+		hb_retl( HB_FALSE );
+}
+
 HB_FUNC( UIMAIN ) {
     uiMain();
 }
